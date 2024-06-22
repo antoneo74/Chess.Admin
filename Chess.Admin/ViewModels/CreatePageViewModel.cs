aa729d03e30225740d0a4d8cd0d104fdb0735014
staunton <staunton@student.21-school.ru>
2024-06-23 04:31:32 +0700

    select from db

--- Chess.Admin/ViewModels/CreatePageViewModel.cs
+++ Chess.Admin/ViewModels/CreatePageViewModel.cs
@@ -1,5 +1,10 @@
+using ChessDB;
 using ReactiveUI;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
 
 namespace Chess.Admin.ViewModels
 {
@@ -11,11 +16,37 @@ namespace Chess.Admin.ViewModels
         private int _score;
         private int _technique;
         private int _grade;
+        private int _count;
+        private ObservableCollection<string> _fenList;
+        private string _message = string.Empty;
+
+        public string Message
+        {
+            get => _message;
+
+            set => this.RaiseAndSetIfChanged(ref _message, value);
+        }
+
+        public ObservableCollection<string> FenList
+        {
+            get { return _fenList; }
+
+            set { this.RaiseAndSetIfChanged(ref _fenList, value); }
+        }
+
+
+        public int Count
+        {
+            get { return _count; }
+
+            set { this.RaiseAndSetIfChanged(ref _count, value); }
+        }
 
         public DateTimeOffset CurrentDate
         {
             get { return _currentDate; }
-            set { this.RaiseAndSetIfChanged(ref _currentDate, value); _dateString = value.ToString(); }
+
+            set { this.RaiseAndSetIfChanged(ref _currentDate, value); }
         }
 
         public int Strategy
@@ -53,22 +84,77 @@ namespace Chess.Admin.ViewModels
             set { this.RaiseAndSetIfChanged(ref _grade, value); }
         }
 
+        public ReactiveCommand<Unit, Unit> Clear { get; }
+
+        public ReactiveCommand<Unit, Unit> Create { get; }
 
 
-        private string _dateString = string.Empty;
-
-        public string DateString
-        {
-            get { return _dateString; }
-            set => this.RaiseAndSetIfChanged(ref _dateString, value);
-        }
 
         public CreatePageViewModel()
         {
             CurrentDate = DateTimeOffset.Now;
 
             Strategy = Tactics = Score = Technique = Grade = -1;
+
+            _fenList = new ObservableCollection<string>();
+
+            Clear = ReactiveCommand.Create(PageClear);
+
+            Create = ReactiveCommand.Create(CreateExercisesList, this.WhenAnyValue(x => x.Count, count => count != 0));
         }
 
+        private void CreateExercisesList()
+        {
+            try
+            {
+                using (ChessDbContext context = new())
+                {
+                    var list = context.Fens.Select(x => x);
+                    if (Strategy != -1)
+                    {
+                        list = list.Where(x => x.Strategy == Strategy);
+                    }
+                    if (Tactics != -1)
+                    {
+                        list = list.Where(x => x.Tactics == Tactics);
+                    }
+                    if (Score != -1)
+                    {
+                        list = list.Where(x => x.Score == Score);
+                    }
+                    if (Technique != -1)
+                    {
+                        list = list.Where(x => x.Technique == Technique);
+                    }
+                    if (Grade != -1)
+                    {
+                        list = list.Where(x => x.Grade == Grade);
+                    }
+
+                    List<string> list2 = new List<string>();
+
+                    //foreach (var item in list)
+                    //{
+                    //    list2.Add(item.Description);
+                    //}
+
+                    FenList = new ObservableCollection<string>(list.Select(x=>x.Description));
+                   
+                }
+            }
+            catch (Exception)
+            {
+                Message = "Что-то пошло не так";
+            }
+        }
+
+        private void PageClear()
+        {
+            CurrentDate = DateTimeOffset.Now;
+
+            Strategy = Tactics = Score = Technique = Grade = -1;
+
+            Count = 0;
+        }
     }
 }
